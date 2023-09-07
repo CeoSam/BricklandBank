da3d6d048a4b774b92b0b8b561cc1e218bc33ac4
CeoSam <144182155+CeoSam@users.noreply.github.com>
2023-09-07 16:48:12 -0600

    Create passwords.js

---
+++ passwords.js
@@ -0,0 +1,29 @@
+function cond(){
+    var p=document.getElementById("nn").value;
+    var u=document.getElementById("mm").value;
+
+   if(p==" "){
+        if(u==" "){
+            return;
+        }
+   }
+   else if(p=="987654321"){
+        if(u=="test"){
+            window.open("file://C:/Users/kicmo/BricklandBankApp/TestBankAccount.html", "_self");
+        }
+        else{
+            window.open ("file://C:/Users/kicmo/BricklandBankApp/invalid.html","_self");
+        }
+   }
+   else if(p=="WaterStory002"){
+        if(u=="Jkizm4321"){
+            window.open("file://C:/Users/kicmo/BricklandBankApp/JordyAccount.html","_self");
+        }
+        else{
+            window.open ("file://C:/Users/kicmo/BricklandBankApp/invalid.html","_self");
+        }
+   }
+   else{
+       window.open ("file://C:/Users/kicmo/BricklandBankApp/invalid.html","_self");
+   }
+}
