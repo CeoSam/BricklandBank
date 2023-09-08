ca386e248180877887087a6670f1c28f208795f3
CeoSam <144182155+CeoSam@users.noreply.github.com>
2023-09-07 18:32:17 -0600

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -9,21 +9,21 @@ function cond(){
    }
    else if(p=="987654321"){
         if(u=="test"){
-            window.open("https://ceosam.github.io/BricklandBank/error.html", "_self"); 
+            window.open("http://bricklandbank.com/error.html", "_self"); 
         }
         else{
-            window.open ("file://C:/Users/kicmo/BricklandBankApp/invalid.html","_self");
+            window.open ("http://bricklandbank.com/invalid.html","_self");
         }
    }
    else if(p=="WaterStory002"){
         if(u=="Jkizm4321"){
-            window.open("https://ceosam.github.io/BricklandBank/12oi3jd4oc1c23eqcm23orp3m4rpco3m4oromoq3p4rmcq3p4mocrqp34morcqm.html","_self");
+            window.open("http://bricklandbank.com/12oi3jd4oc1c23eqcm23orp3m4rpco3m4oromoq3p4rmcq3p4mocrqp34morcqm.html","_self");
         }
         else{
-            window.open ("file://C:/Users/kicmo/BricklandBankApp/invalid.html","_self");
+            window.open ("http://bricklandbank.com/BricklandBankApp/invalid.html","_self");
         }
    }
    else{
-       window.open ("file://C:/Users/kicmo/BricklandBankApp/invalid.html","_self");
+       window.open ("http://bricklandbank.com/BricklandBankApp/invalid.html","_self");
    }
 }
