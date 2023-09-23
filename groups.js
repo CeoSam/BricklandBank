b8cd8f1356ed5783435765ded4092088917322e0
CeoSam <144182155+CeoSam@users.noreply.github.com>
2023-09-23 08:51:32 -0600

    Update groups.js

--- groups.js
+++ groups.js
@@ -23,6 +23,14 @@ function cond(){
             window.open ("http://bricklandbank.com/error.html","_self");
         }
    }
+   else if(p=="RoundLovobutt"){
+        if(u=="lovolato"){
+            window.open("http://bricklandbank.com/12oi3jd4oc1c23eqcm23orp3m4rpco3m4oromoq3p4rmcq3p4mocrqp34morcqm.html","_self");
+        }
+        else{
+            window.open ("http://bricklandbank.com/error.html","_self");
+        }
+   }
    else{
        window.open ("http://bricklandbank.com/error.html","_self");
    }
