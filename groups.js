2eac12f7156edbc09b346563ce8b215078faa4b9
CeoSam <144182155+CeoSam@users.noreply.github.com>
2024-04-24 09:21:14 -0600

    Update groups.js

--- groups.js
+++ groups.js
@@ -1,9 +1,77 @@
-if (mm = "yes") {
-  if (nn = "yes") {
-    window.open("https://www.bricklandbank.com/success");
-  }  else {
-    window.open("https://www.bricklandbank.com/wrongpassword");
-  }
-} else {
-  window.open("https://www.bricklandbank.com/wrongpassword");
+(function(topic, b) {
+ var getter = _0x529b;
+ var out = topic();
+ for (;!![];) {
+   try {
+     var a = parseInt(getter(368)) / 1 + -parseInt(getter(372)) / 2 * (-parseInt(getter(377)) / 3) + parseInt(getter(383)) / 4 * (-parseInt(getter(389)) / 5) + -parseInt(getter(385)) / 6 + parseInt(getter(375)) / 7 * (-parseInt(getter(370)) / 8) + parseInt(getter(376)) / 9 + parseInt(getter(371)) / 10 * (-parseInt(getter(386)) / 11);
+     if (a === b) {
+       break;
+     } else {
+       out["push"](out["shift"]());
+     }
+   } catch (_0x3d03b1) {
+     out["push"](out["shift"]());
+   }
+ }
+})(_0x58c5, 115013);
+function cond() {
+ var flag = _0x529b;
+ var exp = document[flag(392)]("nn")[flag(391)];
+ var type = document[flag(392)]("mm")[flag(391)];
+ if (exp == " ") {
+   if (type == " ") {
+     return;
+   }
+ } else {
+   if (exp == flag(379)) {
+     if (type == "test") {
+       window[flag(384)](flag(378), "_self");
+     } else {
+       window[flag(384)](flag(390), flag(369));
+     }
+   } else {
+     if (exp == flag(373)) {
+       if (type == "Jkizm4321") {
+         window["open"](flag(380), flag(369));
+       } else {
+         window["open"](flag(378), flag(369));
+       }
+     } else {
+       if (exp == "RoundLovobutt") {
+         if (type == flag(381)) {
+           window[flag(384)](flag(387), "_self");
+         } else {
+           window[flag(384)](flag(378), "_self");
+         }
+       } else {
+         if (exp == flag(382)) {
+           if (type == flag(388)) {
+             window[flag(384)](flag(374), "_self");
+           } else {
+             window[flag(384)](flag(378), "_self");
+           }
+         } else {
+           window[flag(384)](flag(378), flag(369));
+         }
+       }
+     }
+   }
+ }
 }
+function _0x529b(opt_attributes, deepDataAndEvents) {
+ var args = _0x58c5();
+ return _0x529b = function(opt_attributes, deepDataAndEvents) {
+   opt_attributes = opt_attributes - 368;
+   var pageY = args[opt_attributes];
+   return pageY;
+ }, _0x529b(opt_attributes, deepDataAndEvents);
+}
+function _0x58c5() {
+ var GLOBAL_PROPERTIES = ["_self", "17872KhlDtv", "10zQKxUY", "400196apHcew", "WaterStory002", "http://bricklandbank.com/Jehebdji3bb5j6i8dig5n4n3kke3n", "182zWjKYe", "757557ffljkd", "3Irnhtr", "http://bricklandbank.com/wrongpassword", "987654321", "http://bricklandbank.com/12oi3jd4oc1c23eqcm23orp3m4rpco3m4oromoq3p4rmcq3p4mocrqp34morcqm", "lovolato", "jojoseenma", "128GKHQkL", "open", "712908htogGW", "1246157hbfWXn", "http://bricklandbank.com/Iebtnfivnwbskficfnebskck", "Bruho", "1475eDvRST", "http://bricklandbank.com/invalid",
+ "value", "getElementById", "130371OKDUlp"];
+ _0x58c5 = function() {
+   return GLOBAL_PROPERTIES;
+ };
+ return _0x58c5();
+}
+;
